82fa97e0fc54b6069e075b115f03935becd8b136
jay <jayhtilwani@gmail.com>
2019-06-15 14:46:45 +0530

    my first commmit

---
+++ src/test/java/com/mycompany/app/SoapAPITest.java
@@ -0,0 +1,18 @@
+package com.mycompany.app;
+
+import org.testng.annotations.Test;
+
+public class SoapAPITest {
+	
+	@Test
+	public void third()
+	{
+		System.out.println("third");
+	}
+	@Test
+	public void four()
+	{
+		System.out.println("four");
+	}
+
+}
